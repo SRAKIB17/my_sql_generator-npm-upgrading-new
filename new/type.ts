ecdf30c0f5feb49187d9569630247dbb38f29b19
SRAKIB17 <rakibulssc5@gmail.com>
2024-03-05 19:52:15 +0600

    'doen

--- new/type.ts
+++ new/type.ts
@@ -1,21 +1,35 @@
-type include = {
-}
+// ! ***************** simpleOperation ************************
 
-export interface includeType {
+interface simpleOperation {
+    "="?: { [field_name: string]: number | string | unknown },
+    ">"?: { [field_name: string]: number | string | unknown },
+    "<"?: { [field_name: string]: number | string | unknown },
+    ">="?: { [field_name: string]: number | string | unknown },
+    "<="?: { [field_name: string]: number | string | unknown },
+    "!="?: { [field_name: string]: number | string | unknown }
+}
+// ! ***************** simpleOperation ************************
+
+// ! ***************** Include ************************
+type $include = { [field_name: string]: string[] | number[] | any[], }
+
+interface includeType {
     "$not_include"?: {
-        "$or"?: { [field_name: string]: string[] | number[] | any[], },
-        "$and"?: { [field_name: string]: string[] | number[] | any[], },
-    } | { [field_name: string]: string[] | number[] | any[], }
+        "$or"?: $include,
+        "$and"?: $include,
+    } | $include
     ,
     "$include"?: {
-        "$or"?: { [field_name: string]: string[] | number[] | any[], },
-        "$and"?: { [field_name: string]: string[] | number[] | any[], },
-    } | { [field_name: string]: string[] | number[] | any[], },
+        "$or"?: $include,
+        "$and"?: $include,
+    } | $include,
 }
 
+// ! ***************** Include ************************
 
+// ! ***************** Patter ************************
 
-type pattern = {
+type $pattern = {
     "$start"?: string | number,
     "$end"?: string | number,
     "$both"?: string | number,
@@ -23,35 +37,51 @@ type pattern = {
 
 interface patternType {
     "$pattern"?: {
-        [field_name: string]: {
-            "$start"?: string | number,
-            "$end"?: string | number,
-            "$both"?: string | number,
-        }
-        |
+        [field_name: string]: $pattern |
         {
             "$or"?: {
-                [field_name: string]: pattern
+                [field_name: string]: $pattern
             },
             "$and"?: {
-                [field_name: string]: pattern
+                [field_name: string]: $pattern
             }
         }
     },
     "$not_pattern"?: {
-        [field_name: string]: {
-            "$start"?: string | number,
-            "$end"?: string | number,
-            "$both"?: string | number,
-        }
-        |
+        [field_name: string]: $pattern |
         {
             "$or"?: {
-                [field_name: string]: pattern
+                [field_name: string]: $pattern
             },
             "$and"?: {
-                [field_name: string]: pattern
+                [field_name: string]: $pattern
             }
         }
     }
 }
+
+// ! ***************** Patter ************************
+// ! ***************** betweenType ************************
+type between = {
+    [field_name: string]: {
+        "$from": string | number,
+        "$to": string | number,
+    }
+}
+
+interface betweenType {
+    "$between"?: between | {
+        "$or"?: between,
+        "$and"?: between
+    },
+}
+// ! ***************** betweenType ************************
+
+
+
+export interface AndOrCondition extends simpleOperation, includeType, patternType, betweenType {
+    "$and": simpleOperation,
+    "$or": simpleOperation,
+    [field_name: string]: unknown
+}
+
